f26147858a4d2976ee872a1344cd3b7d1d3d21f5
parthpranav2 <parthpshroff@gmail.com>
2026-07-24 15:37:36 +0530

    Added one well in simulation

--- mkdir_forresults.m
+++ mkdir_forresults.m
@@ -1,3 +1,5 @@
 runName = datestr(now,'dd_mm_yyyy__HH_MM');
 outputDir = fullfile('/Users/apple/Desktop/study/programming/Matlab/Plugins/MRST-2026a/core/examples/data/Johansen/','outputs',runName);
 mkdir(outputDir);
+fig = 'figures';
+mkdir(fullfile(outputDir,fig));
--- plotssaver.m
+++ plotssaver.m
@@ -1,7 +1,5 @@
 figs = findall(groot,'Type','figure');
-
 for k = 1:numel(figs)
-    savefig(figs(k), fullfile(outputDir,'figures',sprintf('Figure_%02d.fig',k)));
     exportgraphics(figs(k), fullfile(outputDir,'figures',sprintf('Figure_%02d.png',k)), ...
         'Resolution',300);
 end
