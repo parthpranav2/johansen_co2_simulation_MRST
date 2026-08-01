b0d1e6ab9a42f5efba875808ae7f43de13d5e877
parthpranav2 <parthpshroff@gmail.com>
2026-08-01 13:59:24 +0530

    feat: add Johansen simulation outputs and update BO study data and configuration

--- co2lab/co2lab-ve/examples/example3DJohansen.m
+++ co2lab/co2lab-ve/examples/example3DJohansen.m
@@ -1154,6 +1154,9 @@ if exist('SAVE_ONLY', 'var') && SAVE_ONLY && ~HEADLESS
     set(0, 'DefaultFigureVisible', 'on');
 end
 
+% Alert the user that the simulation is complete
+beep; pause(0.5); beep;
+
 %%
 % <html>
 % <p><font size="-1">
